85bd4f54c20cbd91aba9254d6ba3609ce4b3d5f8
byeon-gyuseob <92051660+byeon-gyuseob@users.noreply.github.com>
2022-02-28 01:28:51 +0900

    Delete JpaRepository.java
    not referred

--- src/main/java/com/example/demo/JpaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo;
-
-import org.springframework.data.repository.NoRepositoryBean;
-import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.data.repository.query.QueryByExampleExecutor;
-
-@NoRepositoryBean
-public interface JpaRepository<T, ID>
-        extends PagingAndSortingRepository<T, ID>, QueryByExampleExecutor<T> {}
